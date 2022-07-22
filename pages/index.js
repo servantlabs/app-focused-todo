1c7948f77861adeb782e7a43acc2d7bf39a32c7a
Aaron Bray <aaron@servantlabs.com>
2022-07-22 22:37:10 +0000

    I edited the text on the front page

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="uh oh, watch out now..." />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          The editing has started... 
         </p>
       </main>
 
